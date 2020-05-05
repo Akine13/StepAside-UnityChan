b4847e1d575d856a44d39c7268b2c589d14b1bf6
Akine13 <y20060409@gmail.com>
2020-05-05 21:41:45 +0900

    StepAside UnityChan 走れUnityちゃん!!
    課題提出

---
+++ Assets/ItemDeleter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDeleter : MonoBehaviour
+{
+	private GameObject unitychan;
+	private float z;
+	// Use this for initialization
+	void Start()
+	{
+		z = transform.position.z;
+		unitychan = GameObject.FindGameObjectWithTag("unitychanTag");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (unitychan.transform.position.z >= z + 6)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Script/CoinController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CoinController : MonoBehaviour
+{
+    private GameObject unitychan;
+    private float z;
+    // Use this for initialization
+    void Start()
+    {
+        z = this.transform.position.z;
+        unitychan = GameObject.FindGameObjectWithTag("unitychanTag");
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitychan.transform.position.z >= z + 7)
+        {
+            Destroy(this.gameObject);
+        }
+        this.transform.Rotate(0, 3, 0);
+    }
+}
